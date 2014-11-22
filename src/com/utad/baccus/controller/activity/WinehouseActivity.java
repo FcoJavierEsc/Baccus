4b9e6c310c553a89d8bdf6341a95b98b1e666e41
Francisco Escudero <fcoesctec@gmail.com>
2014-11-22 13:47:13 +0100

    menu anterior siguiente

--- src/com/utad/baccus/controller/activity/WinehouseActivity.java
+++ src/com/utad/baccus/controller/activity/WinehouseActivity.java
@@ -1,20 +1,28 @@
 package com.utad.baccus.controller.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBar.Tab;
 import android.support.v7.app.ActionBar.TabListener;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.utad.baccus.R;
 import com.utad.baccus.controller.adapter.WineFragmentAdapter;
+import com.utad.baccus.controller.fragment.SettingsFragment;
 
 public class WinehouseActivity extends ActionBarActivity {
-	WineFragmentAdapter mAdapter = null;
-	ActionBar mActionBar = null;
+	private WineFragmentAdapter mAdapter = null;
+	private ActionBar mActionBar = null;
+	private MenuItem befItem;
+	private MenuItem nextItem;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +34,7 @@ public class WinehouseActivity extends ActionBarActivity {
 
 		mAdapter = new WineFragmentAdapter(getSupportFragmentManager());
 
-		pager.setAdapter(new WineFragmentAdapter(getSupportFragmentManager()));
+		pager.setAdapter(mAdapter);
 
 		getSupportActionBar().setSubtitle(pager.getAdapter().getPageTitle(0));
 
@@ -41,20 +49,15 @@ public class WinehouseActivity extends ActionBarActivity {
 
 			@Override
 			public void onPageScrolled(int arg0, float arg1, int arg2) {
-				// TODO Auto-generated method stub
-
 			}
 
 			@Override
 			public void onPageScrollStateChanged(int arg0) {
-				// TODO Auto-generated method stub
-
 			}
 		});
 
 		mActionBar = getSupportActionBar();
 		mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		updateActionBar(0);
 
 		for (int i = 0; i < mAdapter.getCount(); i++) {
 			Tab tab = mActionBar.newTab();
@@ -79,12 +82,58 @@ public class WinehouseActivity extends ActionBarActivity {
 
 		}
 
+		updateActionBar(0);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.nextbefore, menu);
+		befItem = menu.findItem(R.id.action_before);
+		nextItem = menu.findItem(R.id.action_next);
+		int index = mActionBar.getSelectedNavigationIndex();
+		
+        befItem.setEnabled(index > 0);
+        nextItem.setEnabled(index<mAdapter.getCount()-1);
+		
+        MenuItemCompat
+				.setShowAsAction(nextItem, MenuItem.SHOW_AS_ACTION_ALWAYS);
+		MenuItemCompat.setShowAsAction(befItem, MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		boolean defaultValue = super.onOptionsItemSelected(item);
+		int actIndex = mActionBar.getSelectedNavigationIndex();
+
+		switch (item.getItemId()) {
+		case R.id.action_next:
+			if (actIndex + 1 < mAdapter.getCount())
+				updateActionBar(actIndex + 1);
+			
+			break;
+
+		case R.id.action_before:
+			if (actIndex > 0)
+				updateActionBar(actIndex - 1);
+			
+			break;
+
+		}
+
+		return defaultValue;
 	}
 
 	private void updateActionBar(int index) {
-		mActionBar = getSupportActionBar();
+		// mActionBar = getSupportActionBar();
 		mActionBar.setSubtitle(mAdapter.getPageTitle(index));
 		mActionBar.setIcon(mAdapter.getImageResource(index));
-
+		mActionBar.setSelectedNavigationItem(index);
 	}
 }
